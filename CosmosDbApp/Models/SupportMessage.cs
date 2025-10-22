e6d938a3344ab8a15d88c2872a37119ce94ecbfb
Lukas <eaa24luba.students@eaaa.dk>
2025-10-22 10:27:09 +0200

    First

---
+++ CosmosDbApp/Models/SupportMessage.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CosmosDbApp.Models
+{
+    public class SupportMessage
+    {
+        [Required]
+        public string Id { get; set; }
+
+        [Required] [StringLength(100)]
+        public string CustomerName { get; set; } = string.Empty; 
+        
+        [Required]
+        public string Email { get; set; } = string.Empty;
+        
+        [Required] 
+        public string PhoneNumber { get; set; } = string.Empty;
+        
+        [Required]
+        [StringLength(100)]
+        public string Description { get; set; } = string.Empty;
+        
+        [Required]
+        public SupportCategory Category { get; set; }
+        
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+
+        public Status Status { get; set; } = Status.NEW; 
+    }
+
+    public enum SupportCategory
+    {
+        TECHNICAL_SETUP, 
+        DIY_PARTS, 
+        FEATURE_REQUEST,
+        FIND_DEALER,
+        REQUEST_CATELOG,
+        OTHER
+    }
+
+    public enum Status
+    {
+        NEW,
+        IN_PROGRESS,
+        RESOLVED,
+        CLOSED
+    }
+    }
+
