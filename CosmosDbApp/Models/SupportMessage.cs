f4f523feb4434a03785e8e298635adca0acfce59
Lukas <eaa24luba.students@eaaa.dk>
2025-10-23 11:11:46 +0200

    Home page og requiredments

--- CosmosDbApp/Models/SupportMessage.cs
+++ CosmosDbApp/Models/SupportMessage.cs
@@ -7,23 +7,23 @@ namespace CosmosDbApp.Models
         
         public string Id { get; set; } = string.Empty;
 
-        [Required (ErrorMessage = "Your name, thanks")] 
-        [StringLength(100, ErrorMessage = "To long, unfortunately")]
+        [Required (ErrorMessage = "Dit navn, thanks")] 
+        [StringLength(100, ErrorMessage = "Alt for langt, unfortunately")]
         public string CustomerName { get; set; } = string.Empty; 
         
-        [Required(ErrorMessage = "Your email, thanks")]
-        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
+        [Required(ErrorMessage = "Din mail, thanks")]
+        [EmailAddress(ErrorMessage = "Vil du være sød at indtaske in vaild email?")]
         public string Email { get; set; } = string.Empty;
         
         [Required]
-        [Phone(ErrorMessage = "Please enter a vaild phone number")]
+        [Phone(ErrorMessage = "Please indtast et gyldigt telefonnummer")]
         public string PhoneNumber { get; set; } = string.Empty;
         
-        [Required]
-        [StringLength(100)]
+        [Required(ErrorMessage = "Please skriv en beskrivelse")]
+        [StringLength(1000, ErrorMessage = "To long, unfortunately")]
         public string Description { get; set; } = string.Empty;
         
-        [Required]
+        [Required(ErrorMessage = "Please enter a support category")]
         public SupportCategory Category { get; set; }
         
         public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
@@ -33,19 +33,29 @@ namespace CosmosDbApp.Models
 
     public enum SupportCategory
     {
+        [Display(Name =" Teknisk opsætning")]
         TECHNICAL_SETUP, 
+        [Display(Name ="Gør det selv reservedele")]
         DIY_PARTS, 
+        [Display(Name ="Forslag til forbedringer")]
         FEATURE_REQUEST,
+        [Display(Name =" Find forhandler")]
         FIND_DEALER,
+        [Display(Name ="Andmod om katalog")]
         REQUEST_CATALOG,
+        [Display(Name =" Andet")]
         OTHER
     }
 
     public enum Status
     {
+        [Display(Name =" Ny")]
         NEW,
+        [Display(Name ="I gang")]
         IN_PROGRESS,
+        [Display(Name ="Løst")]
         RESOLVED,
+        [Display(Name ="Lukket")]
         CLOSED
     }
     }
